a853af04178ca2efd5718302c9238e19b5243b32
searchme86 <searchme86@gmail.com>
2022-10-25 00:22:42 +0900

    ✨ add click event functions

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,9 +6,37 @@ function Content() {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+
+  const handleClick = () => {
+    console.log('you clicked it');
+  };
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleChangeName()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleChangeName()}!</p>
+      <button onClick={handleClick}>Click it</button>
+      <button
+        onClick={() => {
+          handleClick2('rohkore');
+        }}
+      >
+        Click it
+      </button>
+      <button
+        onClick={(e) => {
+          handleClick3(e);
+        }}
+      >
+        Click it
+      </button>
     </main>
   );
 }
